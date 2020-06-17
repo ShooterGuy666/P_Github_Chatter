8b7ad8df02cc026d4ae2ad0bbda799d777159b46
ShooterGuy666 <p.kling98@hotmail.com>
2020-06-17 16:42:43 +0200

    main.js updated
    click combinatie voor chatrooms uitgeschakeld in verband met iframe

--- public/js/main.js
+++ public/js/main.js
@@ -5,26 +5,26 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $('.chatname').click(function(){
-    $('.show').toggleClass('d-none');
-    $('#return').toggleClass('d-none');
-    $('.chatroom').toggleClass('d-none');
-    $('.app_menu').toggleClass('d-none');
-    $('.message_type').toggleClass('d-none');
+// $(document).ready(function(){
+//   $('.chatname').click(function(){
+//     $('.show').toggleClass('d-none');
+//     $('#return').toggleClass('d-none');
+//     $('.chatroom').toggleClass('d-none');
+//     $('.app_menu').toggleClass('d-none');
+//     $('.message_type').toggleClass('d-none');
 
-  });
-});
+//   });
+// });
 
-$(document).ready(function(){
-  $('#return').click(function(){
-    $('.show').toggleClass('d-none');
-    $('#return').toggleClass('d-none');
-    $('.chatroom').toggleClass('d-none');
-    $('.app_menu').toggleClass('d-none');
-    $('.message_type').toggleClass('d-none');
-  });
-});
+// $(document).ready(function(){
+//   $('#return').click(function(){
+//     $('.show').toggleClass('d-none');
+//     $('#return').toggleClass('d-none');
+//     $('.chatroom').toggleClass('d-none');
+//     $('.app_menu').toggleClass('d-none');
+//     $('.message_type').toggleClass('d-none');
+//   });
+// });
 
 
 
