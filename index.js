577c682d3e83af3edb70f70f6d179c64df33b4ae
ShooterGuy666 <p.kling98@hotmail.com>
2020-06-17 16:06:52 +0200

    Added iframe and chat rooms

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ app.get('/css', (req, res) =>{
     res.sendFile(__dirname + '/public/css.html');
 });
 
+app.get('/nightlife', (req, res) =>{
+    res.sendFile(__dirname + '/public/nightlife.html');
+});
+
+app.get('/chillout', (req, res) =>{
+    res.sendFile(__dirname + '/public/chillout.html');
+});
+
+app.get('/moviesseries', (req, res) =>{
+    res.sendFile(__dirname + '/public/series_movies.html');
+});
+
+app.get('/sports', (req, res) =>{
+    res.sendFile(__dirname + '/public/sports.html');
+});
+
 //tech namespace
 const tech = io.of('/tech');
 
@@ -51,6 +67,33 @@ tech.on('connection', (socket) => {
         console.log('user disconnected');
 
         tech.emit('message', 'user disconnected');
+    });
+});
+
+//chill namespace
+const chill = io.of('/chill');
+
+chill.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        chill.in(data.room).emit('message', `new user joined ${data.room} room!`);
+    })
+
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        chill.in(data.room).emit('message', data.msg);
+        var insert_message = {
+            name: "user",
+            room: data.room,
+            text: data.msg
+        };
+        db.insertChats(insert_message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+
+        chill.emit('message', 'user disconnected');
     });
 });
 
